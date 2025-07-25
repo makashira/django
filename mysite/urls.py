3b81f11c5b802656977fc7198f8cc0a990a2745c
makashira <109970510+makashira@users.noreply.github.com>
2025-07-26 00:34:13 +0200

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,21 +1,14 @@
-"""mysite URL Configuration
+# mysite/urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+# Импортируем наши новые представления из views.py
+from . import views # <--- Эту строку нужно добавить или убедиться, что она есть
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Добавляем маршрут для главной страницы
+    path('', views.home, name='home'), # <--- Эту строку нужно добавить
+    # Добавляем маршрут для тестовой страницы Telethon
+    path('test-telethon/', views.test_telethon_connection, name='test_telethon'), # <--- Эту строку нужно добавить
 ]
